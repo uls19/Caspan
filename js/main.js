3c81c20384e0bb53bccf5d5b8cc3c58ec07b0cff
uls19 <uls19@mail.ru>
2024-03-14 19:37:15 +0200

    all-blocks

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ window.addEventListener('load', () => {
     mask.remove();
   }, 600);
 });
+
+window.onload = function () {
+
+  const burgerEl = document.querySelector('.header__top-lang');
+  const menuEl = document.querySelector('.header__top-lang_up');
+  const polEl = document.querySelector('.header__lang');
+  burgerEl.addEventListener("click", function () {
+      menuEl.classList.toggle('lang_show');
+      polEl.classList.toggle('pol_top');
+  });
+
+  const footerYearEl = document.querySelector('.footer__year');
+  let today = new Date();
+  let year = today.getFullYear(); // 2024
+
+  footerYearEl.textContent = year;
+
+}
