7abbb5aa9bafeaa5446c879556a455b33e1c721d
uls19 <uls19@mail.ru>
2024-03-15 16:11:45 +0200

    all_blocks_adaptiv

--- js/main.js
+++ js/main.js
@@ -9,12 +9,43 @@ window.addEventListener('load', () => {
 
 window.onload = function () {
 
-  const burgerEl = document.querySelector('.header__top-lang');
-  const menuEl = document.querySelector('.header__top-lang_up');
+  /*Nav icon*/
+  const burgerEl = document.querySelector('.nav-icon-btn');
+  const navIcon = document.querySelector('.nav-icon');
+  const menuEl = document.querySelector('.header__top-contact');
+
+// вызываем элемент и скрываем если клик был за его пределами
+
+  const toggleMenu = function () {
+  menuEl.classList.toggle("header__top-contact_mobile");
+  navIcon.classList.toggle('nav-icon_active');
+  document.body.classList.toggle('no-scroll');
+
+}
+
+burgerEl.addEventListener("click", function (e) {
+  e.stopPropagation();
+  toggleMenu();
+});
+
+document.addEventListener("click", function (e) {
+  const target = e.target;
+  const its_menu = target == menuEl || menuEl.contains(target);
+  const its_btnMenu = target == burgerEl;
+  const menu_is_active = menuEl.classList.contains("header__top-contact_mobile");
+
+  if (!its_menu && !its_btnMenu && menu_is_active) {
+    toggleMenu();
+  }
+});
+
+
+  const burgerLl = document.querySelector('.header__top-lang');
+  const menuLl = document.querySelector('.header__top-lang_up');
   const polEl = document.querySelector('.header__lang');
-  burgerEl.addEventListener("click", function () {
-      menuEl.classList.toggle('lang_show');
-      polEl.classList.toggle('pol_top');
+  burgerLl.addEventListener("click", function () {
+    menuLl.classList.toggle('lang_show');
+    polEl.classList.toggle('pol_top');
   });
 
   const footerYearEl = document.querySelector('.footer__year');
